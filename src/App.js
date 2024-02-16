232e950b7e8beef4a2c3320ae14390988ea56b75
shruti garg <hellocoders8213@gmail.com>
2024-02-16 16:21:14 +0530

    sidebar styling + toggle functionality on click on menu bars for small screen

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/header/Header";
 import Sidebar from "./components/sidebar/Sidebar";
 import { Container } from "react-bootstrap";
 import HomeScreen from "./screens/homescreen/HomeScreen";
 import "./_app.scss";
 const App = () => {
+	const [sidebar, setSidebar] = useState(false);
+
+	const handleToggleSidebar = () => {
+		setSidebar(!sidebar);
+	};
+
 	return (
 		<>
-			<Header />
+			<Header handleToggleSidebar={handleToggleSidebar} />
 			<div className="app__container border border-info">
-				<Sidebar />
+				<Sidebar sidebar={sidebar} handleToggleSidebar={handleToggleSidebar} />
 				<Container fluid className="app__main border border-warning">
 					<HomeScreen />
 				</Container>
